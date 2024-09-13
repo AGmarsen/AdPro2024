97eafbed4a5ffdb91a4e5f7c1ac5c23025cf72ac
AGmarson <tcla@itu.dk>
2024-09-13 16:04:12 +0200

    week 3

--- 03-option/Exercises.scala
+++ 03-option/Exercises.scala
@@ -3,6 +3,8 @@
 
 package adpro.option
 
+import java.awt.Point
+
 // Exercise 1
 
 trait OrderedPoint 
@@ -11,7 +13,14 @@ trait OrderedPoint
   this: java.awt.Point =>
 
   override def compare(that: java.awt.Point): Int = 
-    ???
+    (that.getX(), that.getY()) match
+      case (x, y) if x < this.getX() => 1
+      case (x, y) if x > this.getX() => -1
+      case (x, y) if x == this.getX() => 
+        if y < this.getY() then 1
+        else if y > this.getY() then -1
+        else 0
+    
 
 // Try the following (and similar) tests in the repl (sbt console):
 //
@@ -32,26 +41,42 @@ object Tree:
 
   // Exercise 2
 
-  def size[A](t: Tree[A]): Int = ???
+  def size[A](t: Tree[A]): Int = 
+    t match
+      case Leaf(_) => 1
+      case Branch(left, right) => 
+        1 + size(left) + size(right)
+    
 
   // Exercise 3
 
-  def maximum(t: Tree[Int]): Int = ???
+  def maximum(t: Tree[Int]): Int = 
+    t match
+      case Leaf(v) => v
+      case Branch(left, right) => maximum(left) max maximum(right)
+    
 
   // Exercise 4
 
-  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = 
+    t match
+      case Leaf(v) => Leaf(f(v))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    
 
   // Exercise 5
 
-  def fold[A,B](t: Tree[A])(f: (B, B) => B)(g: A => B): B = ???
+  def fold[A,B](t: Tree[A])(f: (B, B) => B)(g: A => B): B = 
+    t match
+      case Leaf(v) => g(v)
+      case Branch(left, right) => f(fold(left)(f)(g), fold(right)(f)(g))
 
-  def size1[A](t: Tree[A]): Int =  ???
+  def size1[A](t: Tree[A]): Int =  fold[A,Int](t)(1 + _ + _)(_ => 1)
 
-  def maximum1(t: Tree[Int]): Int = ???
+  def maximum1(t: Tree[Int]): Int = fold[Int,Int](t)(_ max _)(identity)
 
-  def map1[A, B](t: Tree[A])(f: A => B): Tree[B] = ???
-
+  def map1[A, B](t: Tree[A])(f: A => B): Tree[B] = 
+    fold[A,Tree[B]](t)((l, r) => Branch(l, r))(v => Leaf(f(v)))
 
 
 
@@ -61,15 +86,31 @@ enum Option[+A]:
 
   // Exercise 6
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] = this match
+    case None => None
+    case Some(v) => Some(f(v))
+    
+  
 
-  def getOrElse[B >: A] (default: => B): B = ???
+  def getOrElse[B >: A] (default: => B): B = this match
+    case None => default
+    case Some(v) => v
+    
 
-  def flatMap[B](f: A => Option[B]): Option[B] =  ???
+  def flatMap[B](f: A => Option[B]): Option[B] = this match
+    case None => None
+    case Some(v) => f(v)
+  
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match
+    case None => ob
+    case Some(v) => Some(v)
 
-  def filter(p: A => Boolean): Option[A] = ???
+
+  def filter(p: A => Boolean): Option[A] = this match
+    case Some(v) if p(v) => Some(v)
+    case _ => None
+  
 
   // Scroll down for Exercise 7, in the bottom of the file, outside Option
 
@@ -79,23 +120,25 @@ enum Option[+A]:
     
 
 
-
 object Option:
 
   // Exercise 9
 
   def map2[A, B, C](ao: Option[A], bo: Option[B])(f: (A,B) => C): Option[C] =
-    ???
+    for 
+      a <- ao
+      b <- bo
+    yield f(a, b)
 
   // Exercise 10
 
   def sequence[A](aos: List[Option[A]]): Option[List[A]] =
-    ???
+    aos.foldRight(Some(Nil))((e, ac) => ac.flatMap(a => e.map(_::a)))
 
   // Exercise 11
 
   def traverse[A, B](as: List[A])(f: A => Option[B]): Option[List[B]] =
-    ???
+    sequence(as.map(f))
     
 end Option
 
@@ -107,15 +150,18 @@ import Option.{Some, None}
 
 def headOption[A](lst: List[A]): Option[A] = lst match
   case Nil => None
-  case h:: t => Some(h)
+  case h :: t => Some(h)
 
 // Exercise 7
 
 def headGrade(lst: List[(String,Int)]): Option[Int] =
-  ???
+  headOption(lst).map(_._2)
 
 def headGrade1(lst: List[(String,Int)]): Option[Int] =
-  ???
+  for 
+    x <- headOption(lst)
+  yield x._2
+  
 
 // Implemented in the text book
 
@@ -126,6 +172,13 @@ def mean(xs: Seq[Double]): Option[Double] =
 // Exercise 8
 
 def variance(xs: Seq[Double]): Option[Double] =
-  ???
+  for 
+    x1 <- mean(xs)
+    x2 <- mean(xs.map(a => math.pow(a - x1, 2)))
+  yield x2
+  
+  
+
+
  
 // Scroll up, to the Option object for Exercise 9
